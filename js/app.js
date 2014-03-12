45e8bbe0ba83cafdaf6d52e2c233a415a685d6cd
Scthe1311C <192221@student.pwr.wroc.pl>
2014-03-12 13:16:05 +0100

    'Add comment' button working

--- js/app.js
+++ js/app.js
@@ -16,51 +16,81 @@ Settings.setup();
 
 
 // models
-var Aaa = Backbone.Model.extend({
-	urlRoot: '/'
+var app = {}; // app namespace
+app.Comment = Backbone.Model.extend({
+	//urlRoot: '/'
+	defaults: {
+		author: 'Android Developers',
+		avatar: 'imgs/avatar2.jpg',
+		created_at: '11-03-2013',
+		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ornare libero odio, quis dapibus elit vehicula vel. Pellentesque aliquet nec eros quis pellentesque. Etiam placerat iaculis purus, in ullamcorper elit iaculis non. Vivamus volutpat tristique massa quis commodo. Sed eget lectus at elit cursus vestibulum. Nulla consectetur, turpis sit amet mollis feugiat, enim sem dapibus dui, vel feugiat sapien tellus vitae sapien.'
+	}
 });
+
+app.Comments = Backbone.Collection.extend({
+	model: app.Comment,
+	localStorage: new Store("backbone-comments")
+});
+app.comments = new app.Comments();
+// app.comments.on('add', this.addAll, this);
 
 // views
 var TestView = Backbone.View.extend({
 	el: '#main-panel-content',
-	
+
 	events: {
-		'click #aa': 'fun1'
+		'click .video-comment-name': 'name_click_callback'
 	},
-	
-	fun1: function (ev) {
-		console.log("click");
-		router.navigate('b', {
-			trigger: true
-		});
-	},
-	
+
 	initialize: function () {
 		console.log("init");
+		_.bindAll(this, 'render'); // remember: every function that uses 'this' as the current object should be in here
 		this.render();
 	},
-	
+
 	render: function () {
 		console.log("render");
-		this.$el.html("<span id=\"aa\">aaa</span>");
+		this.$el.html("<span id=\"aa\">aaa</span>" + this.$el.html());
+	},
+	
+	name_click_callback: function (ev) {
+		console.log("click");
+		//router.navigate('b', {
+		//	trigger: true
+		//});
 	}
 });
 var testView = new TestView();
 
-var TestView2 = Backbone.View.extend({
-	el: '#main-panel-content',
-	
-	initialize: function () {
-		console.log("init2");
-		this.render();
-	},
-	
+app.CommentView = Backbone.View.extend({
+	//el: '#video-comments',
+
+	tagname: 'li',
+
+	template: _.template($("#comment-template").html()),
+
 	render: function () {
-		console.log("render2");
-		this.$el.html("<span id=\"bb\">bbb</span>");
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
 	}
 });
-var testView2 = new TestView2();
+
+app.CommentListView = Backbone.View.extend({
+	el: "#video-comments",
+
+	initialize: function () {
+		app.comments.on('add', this.addOne, this);
+		app.comments.fetch();
+	},
+
+	addOne: function (comment) {
+		var view = new app.CommentView({
+			model: comment
+		});
+		this.$el.append(view.render().el);
+	}
+});
+app.commentsListView = new app.CommentListView();
 
 // routing
 var Router = Backbone.Router.extend({
@@ -86,28 +116,7 @@ router.on('route:c', function (id) {
 Backbone.history.start();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$("#add_comment").click(function () {
+	console.log("add");
+	app.comments.create(app.Comment.defaults);
+});
