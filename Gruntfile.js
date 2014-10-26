839a252cadc2be94f315d17b38b01e30f38e350d
Scthe <marcin1113C@gmail.com>
2014-10-26 17:03:32 +0100

    Add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,229 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  var config = {
+    app: 'app',
+    dist: 'dist'
+  };
+
+  grunt.initConfig({
+    config: config,
+
+    watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['bowerInstall']
+      },
+      js: {
+        // files: ['<%= config.app %>/scripts/{,*/}*.js'],
+        files: ['js/**/*.js'],
+        tasks: ['jshint'],
+        options: {
+          livereload: true
+        }
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      sass: {
+        files: ['<%= config.app %>/styles/{,*/}*.{scss,sass}'],
+        tasks: ['sass:server', 'autoprefixer']
+      },
+      styles: {
+        files: ['<%= config.app %>/styles/{,*/}*.css'],
+        tasks: ['newer:copy:styles', 'autoprefixer']
+      },
+      jade: {
+        files: ['<%= config.app %>/{,*/}*.jade',
+          '<%= config.app %>/**/{,*/}*.jade'
+        ],
+        tasks: ['jade'],
+        options: {
+          livereload: true
+        }
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= config.app %>/{,*/}*.html',
+          '.tmp/styles/{,*/}*.css',
+          // '.tmp/templates/**/{,*/}*.html',
+          '.tmp/**/{,*/}*.html',
+          '<%= config.app %>/images/{,*/}*'
+        ]
+      }
+    },
+
+    connect: {
+      options: {
+        port: 9000,
+        open: true,
+        livereload: 35729,
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function(connect) {
+            return [
+              connect.static('.tmp'),
+              connect().use('/bower_components', connect.static('./bower_components')),
+              connect.static(config.app)
+            ];
+          }
+        }
+      }
+    },
+
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= config.dist %>/*',
+            '!<%= config.dist %>/.git*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= config.app %>/scripts/{,*/}*.js',
+        '!<%= config.app %>/scripts/vendor/*',
+        'test/spec/{,*/}*.js'
+      ]
+    },
+
+    sass: {
+      options: {
+        includePaths: [
+          'bower_components'
+        ]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/styles',
+          src: ['*.scss'],
+          dest: '.tmp/styles',
+          ext: '.css'
+        }]
+      },
+      server: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/styles',
+          src: ['*.scss'],
+          dest: '.tmp/styles',
+          ext: '.css'
+        }]
+      }
+    },
+
+    bowerInstall: {
+      app: {
+        src: ['<%= config.app %>/index.html'],
+        exclude: ['bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap.js']
+      },
+      sass: {
+        src: ['<%= config.app %>/styles/{,*/}*.{scss,sass}']
+      }
+    },
+
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= config.app %>',
+          dest: '<%= config.dist %>',
+          src: [
+            '*.{ico,png,txt}',
+            '.htaccess',
+            'images/{,*/}*.webp',
+            '{,*/}*.html',
+            'styles/fonts/{,*/}*.*',
+            'scripts',
+          ]
+        }, {
+          expand: true,
+          dot: true,
+          cwd: '.',
+          src: ['bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap/*.*'],
+          dest: '<%= config.dist %>'
+        }]
+      },
+      styles: {
+        expand: true,
+        dot: true,
+        cwd: '<%= config.app %>/styles',
+        dest: '.tmp/styles/',
+        src: '{,*/}*.css'
+      }
+    },
+
+    concurrent: {
+      server: [
+        'sass:server',
+        'copy:styles'
+      ],
+      test: [
+        'copy:styles'
+      ],
+      dist: [
+        'sass',
+        'copy:styles',
+        'imagemin',
+        'svgmin'
+      ]
+    },
+
+    jade: {
+      compile: {
+        options: {
+          data: {
+            debug: false
+          },
+          pretty: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>',
+          src: '**/*.jade',
+          dest: '.tmp',
+          ext: '.html'
+        }]
+      }
+    }
+
+  });
+
+
+  grunt.registerTask('default', function(target) {
+    grunt.task.run([
+      'clean:server',
+      'concurrent:server',
+      // 'autoprefixer',
+      'jade',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+
+};
