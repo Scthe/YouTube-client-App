59b422974b7a9a77d118b08cadd58ebdfc59a8e7
Scthe <marcin1113C@gmail.com>
2014-11-29 20:11:53 +0100

    Use common intendation

--- app/scripts/views/channelListItemView.js
+++ app/scripts/views/channelListItemView.js
@@ -1,51 +1,51 @@
 define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'services/favoriteChannelsService',
-  'text!templates/channelListItem.tmpl.html'
+	'jquery',
+	'underscore',
+	'backbone',
+	'services/favoriteChannelsService',
+	'text!templates/channelListItem.tmpl.html'
 ], function($, _, Backbone, favoriteChannelsService, tmpl) {
 
-  'use strict';
+	'use strict';
 
-  var SearchChannelListItemView = Backbone.View.extend({
-    tagName: 'li',
+	var SearchChannelListItemView = Backbone.View.extend({
+		tagName: 'li',
 
-    events: {
-      'click .action-good': 'addToFavorites',
-      'click .action-bad': 'removeFromFavorites',
-      'click .id-channel': 'goToChannel'
-    },
+		events: {
+			'click .action-good': 'addToFavorites',
+			'click .action-bad': 'removeFromFavorites',
+			'click .id-channel': 'goToChannel'
+		},
 
-    className: 'clearfix content-2-columns list-item-medium margin-bottom-small',
+		className: 'clearfix content-2-columns list-item-medium margin-bottom-small',
 
-    template: _.template(tmpl),
+		template: _.template(tmpl),
 
-    initialize: function() {
-      _.bindAll(this, 'render', 'addToFavorites', 'removeFromFavorites', 'goToChannel');
-      this.render();
-    },
+		initialize: function() {
+			_.bindAll(this, 'render', 'addToFavorites', 'removeFromFavorites', 'goToChannel');
+			this.render();
+		},
 
-    render: function() {
-      this.$el.html(this.template(this.model.toJSON()));
-      return this;
-    },
+		render: function() {
+			this.$el.html(this.template(this.model.toJSON()));
+			return this;
+		},
 
-    addToFavorites: function() {
-      favoriteChannelsService.add(this.model);
-    },
+		addToFavorites: function() {
+			favoriteChannelsService.add(this.model);
+		},
 
-    removeFromFavorites: function() {
-      favoriteChannelsService.remove(this.model);
-    },
+		removeFromFavorites: function() {
+			favoriteChannelsService.remove(this.model);
+		},
 
-    goToChannel: function() {
-      app.router.navigate('channel/{0}'.fmt(this.model.get('id')), {
-        trigger: true
-      });
-    }
+		goToChannel: function() {
+			app.router.navigate('channel/{0}'.fmt(this.model.get('id')), {
+				trigger: true
+			});
+		}
 
-  });
+	});
 
-  return SearchChannelListItemView;
+	return SearchChannelListItemView;
 });
